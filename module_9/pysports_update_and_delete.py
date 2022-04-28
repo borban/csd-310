c54c43f96a2c55ca72d52ae38807693085100e7e
borban <borban@@users.noreply.github.com>
2022-04-28 14:20:27 -0500

    Assignment 9.3

---
+++ module_9/pysports_update_and_delete.py
@@ -0,0 +1,58 @@
+# Ben Orban
+# Assignment 9.3 Pysports Updates and Deletes
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user" : "pysports_user",
+    "password" : "MySQL8IsGreat!",
+    "host" : "127.0.0.1",
+    "database" : "pysports",
+    "raise_on_warnings" : True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+
+    cursor.execute("INSERT INTO player(first_name, last_name, team_id) VALUES('Smeagol', 'Shire Folk', 1)")
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player p INNER JOIN team t ON p.team_id = t.team_id")
+    players = cursor.fetchall()
+
+    print("--DISPLAYING PLAYERS AFTER INSERT--")
+    for player in players:
+        print("Player ID: {}".format(player[0]))
+        print("First Name: {}".format(player[1]))
+        print("Last Name: {}".format(player[2]))
+        print("Team Name: {}\n".format(player[3]))
+
+    cursor.execute("UPDATE player SET team_id=2, first_name='Gollum', last_name='Ring Stealer' WHERE first_name='Smeagol'")
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player p INNER JOIN team t ON p.team_id = t.team_id")
+    players = cursor.fetchall()
+    print("--DISPLAYING PLAYERS AFTER UPDATE--")
+    for player in players:
+        print("Player ID: {}".format(player[0]))
+        print("First Name: {}".format(player[1]))
+        print("Last Name: {}".format(player[2]))
+        print("Team Name: {}\n".format(player[3]))
+    
+    cursor.execute("DELETE FROM player WHERE first_name='Gollum'")
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player p INNER JOIN team t ON p.team_id = t.team_id")
+    players = cursor.fetchall()
+    print("--DISPLAYING PLAYERS AFTER DELETE--")
+    for player in players:
+        print("Player ID: {}".format(player[0]))
+        print("First Name: {}".format(player[1]))
+        print("Last Name: {}".format(player[2]))
+        print("Team Name: {}\n".format(player[3]))
+        
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified datbase does not exist")
+    else:
+        print(err)
+finally:
+    db.close()
